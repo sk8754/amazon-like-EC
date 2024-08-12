b45197d0a96f5cac89d7037793d0476fe1815461
sk8754 <kishi8755@gmail.com>
2024-08-12 19:19:18 +0900

    修正

--- app/resister/page.tsx
+++ app/resister/page.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { signup } from "./actions";
+import Link from "next/link";
 
 const Page = () => {
   return (
     <main>
       <form
         action={signup}
-        className="max-w-[400px] mx-auto text-center h-[500px] "
+        className="max-w-[400px] mx-auto text-center mt-[3rem]"
       >
         <div className="px-[5%] sm:px-0 flex flex-col w-[full] h-[100%] mx-auto justify-center gap-4">
           <h2 className="font-bold text-2xl">登録ページ</h2>
@@ -37,6 +38,14 @@ const Page = () => {
           </button>
         </div>
       </form>
+
+      <div className="max-w-[400px] mx-auto text-center mt-[3rem]">
+        <button className="bg-[#2dbe22] w-[50%] mx-auto text-[white] p-[0.5rem] rounded-[1rem]">
+          <Link className="block" href={"/login"}>
+            ログインページ
+          </Link>
+        </button>
+      </div>
     </main>
   );
 };
